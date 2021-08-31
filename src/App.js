196db07dfe2d030dedeabbe6dfa0ed4c61ae85d9
leng1031 <xu.leng@outlook.com>
2021-08-30 20:50:24 -0400

    version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import RegisterUser from './component/ReigsterUser';
+import ListUser from './component/ListUser';
+//import TestPage from './component/UpdateUser';
+
+class App extends React.Component {
+  render() {
+    return (
+        <div className='container-fluid vh-100 py-4'>
+          <Router>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/register">Register</Link>
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route path="/register">
+                <div className='row h-100  justify-content-center'>
+                  <RegisterUser/>
+                </div>
+              </Route>
+              {/*<Route path="/books/:id">*/}
+              {/*  <div className='row h-100  justify-content-center'>*/}
+              {/*    <TestPage/>*/}
+              {/*  </div>*/}
+              {/*</Route>*/}
+              <Route path="/">
+                <div className='row h-100  justify-content-center'>
+                  <ListUser/>
+                </div>
+              </Route>
+            </Switch>
+          </Router>
+
+        </div>
+    )
+  }
 }
 
-export default App;
+export default App
